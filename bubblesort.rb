e3db52cb35594f0a5a5b80f65faa9e19bdaa5298
edouard winia <edouardwinia@gmail.com>
2016-04-12 22:23:38 +0200

    challenge 2 done without tests

--- bubblesort.rb
+++ bubblesort.rb
@@ -1,29 +1,39 @@
-sequence = [5, 4, 3, 2, 1, 0]
-swaps = 0
-iterations = 0
-# Your Code Here
-i = 0
-
-while i < sequence.size do
-  iterations += 1
-  if sequence[i+1].nil?
-    break
-  elsif sequence[i] > sequence[i+1]
-    temp_var = sequence[i]
-    sequence[i] = sequence[i+1]
-    sequence[i+1] = temp_var
-    if i > 0
-      i -= 1
-    else
-      i = 0
+class BubbleSort
+  def self.sort!(sequence)
+    swaps = 0
+    iterations = 0
+    i = 0
+    while i < sequence.size do
+      iterations += 1
+      if sequence[i+1].nil?
+        break
+      elsif sequence[i] > sequence[i+1]
+        temp_var = sequence[i]
+        sequence[i] = sequence[i+1]
+        sequence[i+1] = temp_var
+        if i > 0
+          i -= 1
+        else
+          i = 0
+        end
+        swaps += 1
+      else
+        i += 1
+      end
     end
-    swaps += 1
-  else
-    i += 1
+  end
+
+  def self.sort(sequence)
+    sequence_copy = sequence.dup
+    self.sort!(sequence_copy)
+    return sequence_copy
   end
 end
 
+# a = [4, 3, 5, 0, 1]
+# BubbleSort.sort(a)
+# puts "Final result: #{a}"
 
-puts "Final result: #{sequence}"
-puts "Swaps: #{swaps}"
-puts "Iterations: #{iterations}"
+# puts "Final result: #{sequence}"
+# puts "Swaps: #{swaps}"
+# puts "Iterations: #{iterations}"
